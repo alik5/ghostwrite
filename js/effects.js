f5d3aa580a59a08a35d309a724fd1a6313f3c0b1
Ali FitzGerald <alikfitz@gmail.com>
2014-09-30 20:17:14 -0400

    typed.js and node

--- js/effects.js
+++ js/effects.js
@@ -1,36 +1,30 @@
 
 
-  //$("#top").mouseover(function(){
-  //$("#main-logo").fadeOut(7000,function() {
-   // $("#main-logo").fadeIn(3000,function() {
-      //$("#header").fadeIn(2000,function() {
-        //$("#header").fadeOut(1500, function () {
-          //$(this).remove();
-        //$("#header2").fadeIn(2000, function () {
-          //$("#header2").fadeOut(2000, function() {
-            //$(this).remove();
-            //$("#header3").fadeIn(2000, function () {
-              //$("#header3").fadeOut(2000);
-            //});
-          //});
-        //});
-      //});
- //   });
- // });
-//});
+  $(function(){
+      $(".element").typed({
+        strings: ["Go on then, pick a category.", "It's not hard...", "What are you writing for?"],
+        typeSpeed: 100
+      });
+  });
 
-  $(document).ready(function() {
-  var menu = $('.centered-navigation-menu');
-  var menuToggle = $('.centered-navigation-menu-button');
-  var signUp = $('.sign-up');
 
-  $(menuToggle).on('click', function(e) {
-    e.preventDefault();
-    menu.slideToggle(function(){
-      if(menu.is(':hidden')) {
-        menu.removeAttr('style');
-      }
+
+
+
+  $('#splash_page').mouseover(function(){
+  $(".period").fadeOut(1000,function() {
+  $(".period").fadeIn(1000,function() {
+
     });
   });
 });
+
+
+
+
+
+  
+
+
+
 
