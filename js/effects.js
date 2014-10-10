f670e8cec5972327ec661014323df38c2d2fcea0
Ali FitzGerald <alikfitz@gmail.com>
2014-10-10 18:10:42 -0400

    added ajax and node

--- js/effects.js
+++ js/effects.js
@@ -8,9 +8,6 @@
   });
 
 
-
-
-
   $('#splash_page').mouseover(function(){
   $(".period").fadeOut(1000,function() {
   $(".period").fadeIn(1000,function() {
@@ -19,6 +16,32 @@
   });
 });
 
+
+
+
+$(document).on('open', '.remodal', function () {
+    console.log('open');
+});
+
+$(document).on('opened', '.remodal', function () {
+    console.log('opened');
+});
+
+$(document).on('close', '.remodal', function () {
+    console.log('close');
+});
+
+$(document).on('closed', '.remodal', function () {
+    console.log('closed');
+});
+
+$(document).on('confirm', '.remodal', function () {
+    console.log('confirm');
+});
+
+$(document).on('cancel', '.remodal', function () {
+    console.log('cancel');
+});
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-
-var sys = require("sys");  
-sys.puts("Hello World"); 
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange= function() {
+    if (xhr.readyState!==4) {
+    document.getElementById('ajax').innerHTML = xhr.responseText;
+		}
+	};
+	xhr.open('GET', 'business.html');
+	function sendAjax() {
+	xhr.send();
+	document.getElementById('button').style.display = "none";
+}
